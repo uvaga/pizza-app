dd7006dbd2bbdd97ad2f87760f590de856164000
Andrey A <andrey@uvaga.by>
2026-01-11 01:05:57 +0300

    Added NavLink component

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,8 +1,10 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
+import cn from 'classnames';
 
-export function Layout() {
+export function Layout() {	
+
 	return <div className={styles['layout']}>
 		<div className={styles['sidebar']}>
 			<div className={styles['user']}>
@@ -11,11 +13,15 @@ export function Layout() {
 				<div className={styles['email']}>alari@ya.ru</div>
 			</div>
 			<div className={styles['menu']}>
-				<Link to='/' className={styles['link']}>
+				<NavLink to='/' className={({isActive}) => cn(styles['link'], {
+					[styles.active]: isActive
+ 				})}>
 					<img src="/menu-icon.svg" alt="Иконка меню" />
-					Меню</Link>
-				<Link to='/cart' className={styles['link']}>
-					<img src="/cart-icon.svg" alt="Иконка корзины" />Корзина</Link>
+					Меню</NavLink>
+				<NavLink to='/cart' className={({isActive}) => cn(styles['link'], {
+					[styles.active]: isActive
+ 				})}>
+					<img src="/cart-icon.svg" alt="Иконка корзины" />Корзина</NavLink>
 			</div>
 			<Button className={styles['exit']}>
 				<img src="/exit-icon.svg" alt="Иконка выхода" />
